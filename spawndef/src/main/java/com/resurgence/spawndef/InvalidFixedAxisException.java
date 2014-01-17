e8d2e4e65b4367c059ad3d73d30dff9c957a8777
Joshua <Joshua@BELLDANDY>
2014-01-17 09:17:47 -0500

    Initial commit

---
+++ spawndef/src/main/java/com/resurgence/spawndef/InvalidFixedAxisException.java
@@ -0,0 +1,31 @@
+package com.resurgence.spawndef;
+
+public class InvalidFixedAxisException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4976783486365663356L;
+
+	public InvalidFixedAxisException() {
+		super();
+	}
+
+	public InvalidFixedAxisException(String arg0) {
+		super(arg0);
+	}
+
+	public InvalidFixedAxisException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public InvalidFixedAxisException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public InvalidFixedAxisException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
