949216b1c92fbd74995f1ecccf32de27bda47456
Joshua <Joshua@BELLDANDY>
2014-01-17 09:31:25 -0500

    added copy constructor, double constructor, string constructor

--- spawndef/src/main/java/com/resurgence/spawndef/Time.java
+++ spawndef/src/main/java/com/resurgence/spawndef/Time.java
@@ -1,6 +1,7 @@
 package com.resurgence.spawndef;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
  *    
@@ -18,15 +19,55 @@ import java.util.ArrayList;
 public class Time extends Command {
 	public static final String TIME_COMMAND = "TIME";
 	private ArrayList<String> data;
-	public Time(String time) {
+	public Time() {
 		super();
 		name = TIME_COMMAND;
 		data = new ArrayList<String>();
-		// enforce formatting
-		double timeD = Double.parseDouble(time); 
-		time = String.format("%1$.2f", timeD);
-		data.add(time);
+		data.add("0");
 	}
+	
+	/**
+	 * Constructor accepts a single time value, sets everything else to 0
+	 * 
+	 * @param time
+	 */
+	public Time(double time) {
+		this();
+		setTime(time);
+	}
+	
+	
+	/**
+	 * String constructor - expects a string like
+	 * "0   33   TIME 11.00"
+	 * from the demorecord format with the values for
+	 * timeincrement_ticks entity TIME time_double
+	 * 
+	 * @param s
+	 */
+	public Time(String s) {
+		this();
+		parse(s);
+	}
+	
+	/**
+	 * copy Constructor
+	 * @param t
+	 */
+	public Time(Time t) {
+		this(t.toString());
+	}
+	
+	protected void parse(String string) {
+		try (Scanner scanner = new Scanner(string)) {
+			timeIncrement = scanner.nextInt();
+			entity = scanner.next();
+			name = scanner.next();
+			setTime(scanner.nextDouble());
+		}
+	}
+	
+	
 	@Override
 	public String dataToString() {
 		String s = "";
@@ -36,6 +77,23 @@ public class Time extends Command {
 			}
 		}
 		return s;
+	}
+
+	public String getTime() {
+		return data.get(0);
+	}
+	public void setTime(String time) {
+		data.remove(0);
+		// enforce formatting
+		double timeD = Double.parseDouble(time); 
+		time = String.format("%1$.2f", timeD);
+		data.add(time);
+	}
+	public void setTime(double time) {
+		data.remove(0);
+		// enforce formatting
+		String timeS = String.format("%1$.2f", time);
+		data.add(timeS);
 	}
 
 }
--- spawndef/src/test/java/com/resurgence/spawndef/TimeTest.java
+++ spawndef/src/test/java/com/resurgence/spawndef/TimeTest.java
@@ -6,16 +6,25 @@ public class TimeTest {
 
 	@org.junit.Test
 	public void timeArgCommand() {
-		Time t = new Time("10.00");
+		Time t = new Time(10.00);
 		String expected = "0   0   TIME 10.00";
 		assertEquals(expected, t.toString());
 	}
 
 	@org.junit.Test
 	public void enforceFormat() {
-		Time t = new Time("11");
+		Time t = new Time(11);
 		String expected = "0   0   TIME 11.00";
 		assertEquals(expected, t.toString());
 	}
-	
+
+	@org.junit.Test
+	public void copyConstructor() {
+		Time t = new Time(11);
+		Time t2 = new Time(t);
+		assertEquals(t.toString(), t2.toString());
+		String expected = "0   0   TIME 11.00";
+		assertEquals(expected, t2.toString());
+	}
+
 }
